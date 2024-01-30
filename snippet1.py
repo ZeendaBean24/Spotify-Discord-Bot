4340b0fdd8f4f57c4823e97b55a9274072b64b5a
ZeendaBean24 <liuzeen999@gmail.com>
2024-01-30 10:10:40 -0500

    Add ENV for actual Spotify client ID, secret ID and Redirect URI

--- snippet1.py
+++ snippet1.py
@@ -1,10 +1,19 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import os
+from dotenv import load_dotenv
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='id',
-                                               client_secret='secret',
-                                               redirect_uri='uri',
+# Load environment variables from .env file
+load_dotenv()
+
+# Get your Spotify API credentials from environment variables
+client_id = os.getenv("SPOTIPY_CLIENT_ID")
+client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
+redirect_uri = os.getenv("SPOTIPY_REDIRECT_URI")
+
+# Initialize the Spotipy client with your credentials and required scope
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                               client_secret=client_secret,
+                                               redirect_uri=redirect_uri,
                                                scope='playlist-read-private'))
-
-user_playlists = sp.current_user_playlists()
 
